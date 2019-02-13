14661d0f1de354e4e599f77eb919209d4123049b
crawsome <c3g6b2@gmail.com>
2019-02-13 08:29:19 -0800

    added pretty graphing

---
+++ README.md
@@ -0,0 +1,30 @@
+# Graphtice
+Pronounced like "Practice" but with "Graph" at the beginning. It's a better pun in concept than it rolls off the tongue.
+
+This is an educational resource that I've been working on in my free time. I plan on adding more features and improving on it over time.
+
+## What does it do?
+
+It runs through [Lewis Carrol's Word Ladder Problem](https://en.wikipedia.org/wiki/Word_ladder) and [graphs it](https://i.imgur.com/qR33noj.png)
+
+I'm currently still working on this project to make a more optimal way of graphing. It's currently drawing a sine wave.
+
+I made this because I was learning about fundamentals in my free time:
+* Traversal of nodes
+* Graphing an attractive representation of the data
+* Implementing any solution to the "Wordladder" problem.
+* Exporting a graph to a PDF or image
+
+Future updates will improve all features :)
+
+### Note:
+Script needs write access to directory to save the PDF and the PNG of the graph, and read access to read the dictionary file.
+
+
+# If you liked it:
+
+Check out my other projects! Consider me for a job as a junior programmer (available in Q4 2019)
+
+# About the Author
+
+https://linkedin.com/in/colingburke
--- wordladder.py
+++ wordladder.py
@@ -1,14 +1,19 @@
 from datetime import datetime
-import sys, subprocess, math
+import sys
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
+
 startTime = datetime.now()
+
 
 def timestamp():
     return str(datetime.now() - startTime)
 
 
 class Node(object):
+    """your standard node structure"""
+
     def __init__(self, data=None, id=None):
         self.data = data
         self.id = id
@@ -16,11 +21,7 @@ class Node(object):
         self.neighbors = dict()
 
     def printnode(self):
-        print('\nPRINTING\nGraph Size:\t' + str(self.graph_size))
-        for node in self.nodes:
-            print(node)
-
-    def printnode(self):
+        """printNode: prints node's data"""
         print('data:' + '\t\t\t' + str(self.data))
         print('id:' + '\t\t\t\t' + str(self.id))
         print('visited:' + '\t\t' + str(self.visited))
@@ -35,14 +36,19 @@ class Graph(object):
         self.graph_size = 0
         self.found = False
 
-    def newNode(self, node_data):
+    def new_node(self, node_data):
         """addNode: Adds a unique new node to an undirected graph, increases graph size"""
-        newnode = Node(data=node_data, id=self.graph_size)
+        new_node = Node(data=node_data, id=self.graph_size)
         self.graph_size += 1
-        return newnode
+        return new_node
+
+    def remove_node(self):
+        """remove_node"""
+        pass
 
     def wordladder(self, starting_node, ending_node, wordlist):
         """wordladder: creates a whole path of nodes between one word and another.
+        This is not efficient, and I would love some input on making it work better
         """
         starting_word = str(starting_node.data)
         ending_word = str(ending_node.data)
@@ -52,28 +58,29 @@ class Graph(object):
             starting_node.visited = False
         if self.found: return
         if (starting_word == ending_word):
-            print('\n[S] MOVING DOWN TO:')
+            # print('\n[S] MOVING DOWN TO:')
             self.found = True
-            starting_node.visited = True
+            # starting_node.visited = True
             ending_node.id = self.graph_size
-            ending_node.printnode()
+            # ending_node.printnode()
         else:
             for word, used in zip(wordlist.keys(), wordlist.values()):
                 word_end_delta = self.word_delta(word, ending_word)
                 start_word_delta = self.word_delta(starting_word, word)
-                if word_end_delta[0] >=1 and start_word_delta[0] == 3 and not used and not self.found:
+                if word_end_delta[0] >= 1 and start_word_delta[0] == 3 and not used and not self.found:
                     wordlist[word] = True
-                    newnode = self.newNode(word)
-                    newnode.id = starting_node.id + 1
-                    starting_node.neighbors[word] = newnode
-                    newnode.neighbors[starting_word] = starting_node
+                    new_node = self.new_node(word)
+                    new_node.id = starting_node.id + 1
+                    starting_node.neighbors[word] = new_node
+                    new_node.neighbors[starting_word] = starting_node
                     sys.setrecursionlimit(sys.getrecursionlimit() + 1)
-                    print('MOVING UP TO:')
-                    newnode.printnode()
-                    self.wordladder(newnode, ending_node, wordlist)
-                    print('MOVING DOWN TO:')
+                    # for debugging / demonstration of algorithm steps
+                    # print('MOVING UP TO:')
+                    # new_node.printnode()
+                    self.wordladder(new_node, ending_node, wordlist)
+                    # print('MOVING DOWN TO:')
                     starting_node.visited = True
-                    starting_node.printnode()
+                    # starting_node.printnode()
 
     def word_delta(self, word1, word2):
         """returns an integer of how different 2 words are. 4=the same"""
@@ -86,67 +93,90 @@ class Graph(object):
                 swappable_letter_slot = index
         return likeness, swappable_letter_slot
 
+    def make_graph_layout(self, layout_type):
+
+        pass
+
 
 # This test implements the word ladder problem
-def main():
-
+def main(*args):
     # TIMING TIME
     startTime = datetime.now()
 
     # DATA TIME
     ourdictpath = './words2.txt'
     wordlist = dict()
-    word1 = 'fool'
-    word2 = 'sage'
+    word1 = args[0]
+    word2 = args[1]
     starting_node = Node(word1)
     starting_node.id = 0
     last_node = Node(word2)
-
     g = Graph()
     with open(ourdictpath) as f:
         for line in f:
             line = line.strip('\n').lower()
             if len(word1) == len(line):
                 wordlist[line] = False
-
     g.wordladder(starting_node, last_node, wordlist)
     print(str(datetime.now() - startTime))
     print('Total Nodes made:' + str(g.graph_size))
-    print('Dictionary Size / Words Used = ' + str(len(g.path)/len(wordlist)))
-
+    print('Dictionary Size : ' + str(len(wordlist)) + ' Words Used = : ' + str(len(g.path)))
 
     # GRAPHING TIME
-    # create the graph object
-    ourplot = nx.DiGraph()
+    # populate our edges for new graph we're about to make
     edges = []
     edgedata = list(g.path.values())
     for index, node in enumerate(edgedata):
         try:
-            edges.append(tuple((edgedata[index], edgedata[index+1])))
+            edges.append(tuple((edgedata[index], edgedata[index + 1])))
         except IndexError:
-            print(edges)
+            # print(edges)
             continue
 
-    # nodedata = g.path.values()
-    # This command is powerful. takes [('fool', 'sool'),('sool', 'siol'),(siol, sial)...] and makes our graph data
+    # create the graph object, and add our edges to it.
+    ourplot = nx.DiGraph()
     ourplot.add_edges_from(edges)
 
-    distinquished_nodes = {word1:200,
-               word2:200}
+    # make start and finish nodes be green (representative of go) and red (representative of stop)
+    distinguished_nodes = {word1: 'g', word2: 'r'}
 
-    color_values = [distinquished_nodes.get(node, 100) for node in ourplot.nodes()]
-
+    # create a color list, set color to all other nodes to yellow
+    color_values = [distinguished_nodes.get(node, 'y') for node in ourplot.nodes()]
     green_edges = [edges[0], edges[-1]]
 
+    our_pos = dict()
+    our_scale = 2000
+    nodecount = len(g.path.values())
+    # not needed yet
+    # originx = nodecount / 2
+    originy = our_scale / 2
 
-    pos = nx.spring_layout(ourplot, k=.01)
-    nx.draw_networkx_nodes(ourplot, pos, node_color=color_values, node_size=len(word1)*125)
-    nx.draw_networkx_labels(ourplot, pos)
-    nx.draw_networkx_edges(ourplot, pos, edgelist=edges, edge_color='b', arrows=True)
-    nx.draw_networkx_edges(ourplot, pos, edgelist=green_edges, edge_color='g', arrows=True)
+    # pos = nx.spring_layout(ourplot, scale=our_scale)
+    # This loop replaces the line above ^ since nxgraphs needs more custom layouts!
+    for x in range(0, our_scale):
+        for y, word in enumerate(g.path.values()):
+            # increment our x coordinates evenly
+            x = int(x + (our_scale / nodecount))
+
+            # an alternate graph, makes a beautiful helix shape! Make your own!
+            # y = np.sin(np.sin(x)) + originy
+
+            # The closest I could get to a pretty sine wave (a correction to a mistake in my flow control maybe?)
+            y = np.sin(np.sqrt(x)) + originy
+
+            # add to layout (numpy array) (This is all a layout is!)
+            our_pos[word] = np.array([float(x), float(y)])
+
+    nx.draw_networkx_labels(ourplot, our_pos, font_size=8)
+    nx.draw_networkx_edges(ourplot, our_pos, edgelist=edges, edge_color='b', arrows=True)
+    nx.draw_networkx_edges(ourplot, our_pos, edgelist=green_edges, edge_color='g', arrows=True)
+    nx.draw_networkx_nodes(ourplot, our_pos, node_color=color_values, node_size=len(word1) * 75)
+
+
+    plt.savefig('out.pdf',bbox_inches='tight')
+    plt.savefig('out.png')
     plt.show()
-    plt.savefig('out.pdf')
 
 
 if __name__ == '__main__':
-    main()
+    main('fool', 'sage')
